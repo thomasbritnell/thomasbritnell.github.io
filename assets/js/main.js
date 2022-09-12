99653b406a3f592b58e39f8f718d01b7d1a3e21b
Thomas Britnell <britnelt@mcmaster.ca>
2022-09-12 15:50:22 -0400

    updates to website

--- assets/js/main.js
+++ assets/js/main.js
@@ -173,18 +173,29 @@
    */
   window.addEventListener('load', () => {
     let portfolioContainer = select('.portfolio-container');
+
     if (portfolioContainer) {
       let portfolioIsotope = new Isotope(portfolioContainer, {
         itemSelector: '.portfolio-item'
       });
-
+      
       let portfolioFilters = select('#portfolio-flters li', true);
+
+     
+
+      portfolioIsotope.arrange({filter: '.filter-app'});
+      
+      portfolioIsotope.on('arrangeComplete', function() {
+        AOS.refresh()
+      });
 
       on('click', '#portfolio-flters li', function(e) {
         e.preventDefault();
         portfolioFilters.forEach(function(el) {
           el.classList.remove('filter-active');
         });
+
+
         this.classList.add('filter-active');
 
         portfolioIsotope.arrange({
